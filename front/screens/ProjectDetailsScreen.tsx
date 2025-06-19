ddf3ba53242dc0e6aed698f903c4c5633cf01550
Spatulox <M78stormtrooper@laposte.net>
2025-06-19 15:14:47 +0200

    Moves ProjectDetails to screens
    Moves the ProjectDetails component from the components folder to the screens folder.

--- front/screens/ProjectDetailsScreen.tsx
+++ front/screens/ProjectDetailsScreen.tsx
@@ -3,4 +3,6 @@ import { useRoute } from '@react-navigation/native';
 export default function ProjectDetail() {
   const route = useRoute();
   const { id } = route.params as { id: string };
+
+  return <h1>Project Details</h1>
 }
